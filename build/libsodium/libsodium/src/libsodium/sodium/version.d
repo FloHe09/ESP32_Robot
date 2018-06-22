694443e2609caa615464f1dc2ab796c3cf9813cf
Florian Hermes <flo@Florians-iMac.fritz.box>
2018-06-22 23:51:00 +0200

    buid folder removed

--- build/libsodium/libsodium/src/libsodium/sodium/version.d
+++
@@ -1,9 +0,0 @@
-libsodium/src/libsodium/sodium/version.o: \
- /Users/flo/esp/esp-idf/components/libsodium/libsodium/src/libsodium/sodium/version.c \
- /Users/flo/esp/esp-idf/components/libsodium/port_include/sodium/version.h \
- /Users/flo/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h
-
-/Users/flo/esp/esp-idf/components/libsodium/port_include/sodium/version.h:
-
-/Users/flo/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h:
-/Users/flo/esp/esp-idf/components/libsodium/libsodium/src/libsodium/sodium/version.c:
